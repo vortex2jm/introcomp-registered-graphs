bbb1d1c5ae3d6e2afe4f1a22c1ed2b3d523db8f9
vortex2jm <joaopaulomoura5328@gmail.com>
2022-07-25 15:13:40 -0300

    :sparkles: Adding script code

---
+++ analise.py
@@ -0,0 +1,36 @@
+#Análise das inscrições e matrículas do introcomp por série
+#Desenvolvido por João Paulo Moura Clevelares
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Análise de todos os inscritos===================================#
+serie_column  = pd.read_csv('./inscricoes_2022.csv', usecols=[9])
+
+insc_serie_list = []
+aux = "Ensino Médio)"
+
+for i in serie_column.itertuples():
+    content = str(i[1]).split("(")
+    if content[1] == aux:
+        insc_serie_list.append(content[0])
+print("quantidade de inscritos do ensino médio ->",len(insc_serie_list))
+
+plt.hist(insc_serie_list, bins=20)
+plt.show()
+
+#Análise dos matriculados=========================================#
+
+matriculados = pd.read_csv('./matriculas_2022.csv', usecols=[2,5])
+
+mat_serie_list = []
+
+for i in matriculados.itertuples():
+    if not pd.isnull(i[1]):
+        content = str(i[2]).split("(")
+        if content[1] == aux:
+            mat_serie_list.append(content[0])
+
+print("quantidade de matriculados do ensino médio ->",len(mat_serie_list))
+
+plt.hist(mat_serie_list,bins=20)
+plt.show()
